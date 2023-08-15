7296688991813d904c5b434aec46a0501366b398
Andres Jejen <andresjtdev@gmail.com>
2023-08-15 13:26:05 -0500

    update

--- conversor.py
+++ conversor.py
@@ -4,38 +4,46 @@ Bienvenido al conversor de monedas 💰💰🤑
 1 - Pesos Colombianos
 2 - Pesos argentinos
 3 - Pesos Mexicanos
+4 - Salir
 
 Elige una opcion: """
 
-opcion = int(input(Menu))
-if opcion == 1:
-    dolares = input("¿Cuantos dolares tienes?: ")
-    dolares = float(dolares)
-    valor_peso_colombiano = 3758.60
-    peso_Colombiano = dolares * valor_peso_colombiano
-    peso_Colombiano = round(peso_Colombiano, 2)
-    peso_Colombiano = str(peso_Colombiano)
-    print("Tienes $ " + peso_Colombiano + " Col")
-elif opcion == 2:
-    dolares = input("¿Cuantos dolares tienes?: ")
-    dolares = float(dolares)
-    valor_peso_Argentino = 114.08
-    peso_Argentino = dolares * valor_peso_Argentino
-    peso_Argentino = round(peso_Argentino, 2)
-    peso_Argentino = str(valor_peso_Argentino)
-    print("Tienes $ " + peso_Argentino + " Arg")
 
-elif opcion == 3:
-    dolares = input("¿Cuantos dolares tienes?: ")
-    dolares = float(dolares)
-    valor_peso_Mexicano = 20.16
-    peso_Mexicano = dolares * valor_peso_Mexicano
-    peso_Mexicano = round(peso_Mexicano, 2)
-    peso_Mexicano = str(peso_Mexicano)
-    print("Tienes $ " + peso_Mexicano + " Mex")
 
-else:
-    print('Ingresa una opcion correcta por favor')
+while True:
+    try:
+        opcion = int(input(Menu))
+        if opcion == 1:
+            dolares = float(input("¿Cuantos dolares tienes?: "))
+            valor_peso_colombiano = 4060.79
+            peso_Colombiano = dolares * valor_peso_colombiano
+            peso_Colombiano = round(peso_Colombiano, 2)
+            print("Tienes $ " + str(peso_Colombiano) + " Col")
+
+        elif opcion == 2:
+            dolares = float(input("¿Cuantos dolares tienes?: "))
+            valor_peso_Argentino = 114.08
+            peso_Argentino = dolares * valor_peso_Argentino
+            peso_Argentino = round(peso_Argentino, 2)
+            print("Tienes $ " + str(peso_Argentino) + " Arg")
+
+
+        elif opcion == 3:
+            dolares = float(input("¿Cuantos dolares tienes?: "))
+            valor_peso_Mexicano = 20.16
+            peso_Mexicano = dolares * valor_peso_Mexicano
+            peso_Mexicano = round(peso_Mexicano, 2)
+            print("Tienes $ " + str(peso_Mexicano) + " Mex")
+
+        elif opcion == 4: 
+            print("Adios")
+            break
+
+        else:
+            print("Elija una opcion del menu")
+
+    except:
+        print('Ingresa una opcion correcta por favor')
 
 
 
